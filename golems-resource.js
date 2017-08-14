f7b1c49db524f0616ad0508e8b70c3309ac67dc8
Ken Woodruff <ken.woodruff@gmail.com>
2017-08-14 13:56:51 -0700

    get rid of old junk

--- golems-resource.js
+++ golems-resource.js
@@ -1,21 +1,11 @@
+// TODO: switch to api.golems.io if/when can enable SNI SSL on heroku (requires paid dynos) or switch to lambda or whatever
 angular.module('api', ['ngResource'])
+  .factory('Schema', function($resource) {
+      var Schema = $resource('//golems.herokuapp.com/schema/:aspect');
+      return Schema;
+  })
   .factory('Person', function($resource) {
-      // TODO: switch to api.golems.io if/when can enable SNI SSL on heroku (requires paid dynos) or switch to lambda or whatever
       var Person = $resource('//golems.herokuapp.com/person/:spore');
       return Person;
-    })
+  })
 ;
-
-/*
-ff.factory('Node', function($resource) {
-      var Node = $resource('/nodes/:snid/:action', {snid: "@snid"}, {
-        activate: {method:'PUT', params: {action: 'activate'}},
-        pressButton: {method:'PUT', params: {action: 'pressButton'}},
-        deactivate: {method:'PUT', params: {action: 'daectivate'}},
-        shake: {method:'PUT', params: {action: 'shake'}},
-        getWet: {method:'PUT', params: {action: 'getWet'}},
-        dryOff: {method:'PUT', params: {action: 'dryOff'}}
-      });
-      return Node;
-    });
-*/
