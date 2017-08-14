c04d9fea19dd55a68c68a4471f7c7bf226305ade
Ken Woodruff <ken.woodruff@gmail.com>
2017-08-14 12:44:03 -0700

    point to api.golems.io

--- golems-resource.js
+++ golems-resource.js
@@ -1,21 +1,7 @@
 angular.module('api', ['ngResource'])
   .factory('Person', function($resource) {
-      var Person = $resource('/api/person/:spore.json');
+      var Person = $resource('//api.golems.io/person/:spore');
       return Person;
-    })
-  .factory('Account', function($resource) {
-      var Account = $resource('http://api.golems.io/v1/accounts/:email/:children', { email: "@email" }, { 
-          clients: { method: 'GET', isArray: true, params: { children: 'clients' } },
-          devices: { method: 'GET', isArray: true, params: { children: 'devices' } },
-          gateways: { method: 'GET', isArray: true, params: { children: 'gateways' } }
-        });
-      return Account;
-    })
-  .factory('Gateway', function($resource) {
-      var Gateway = $resource('http://api.golems.io/v1/gateways/:snid/:action', { action: "", snid: "@snid" }, {
-        unpair: { method: 'POST', params: { action: "unpair", snid: "@snid" } }
-      });
-      return Gateway;
     })
 ;
 
